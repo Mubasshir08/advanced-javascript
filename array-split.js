186f86c302462c791be497e6323fa3bf8b1067fb
Mubasshir08 <msr4212019@gmail.com>
2022-07-16 03:16:30 +0600

    split of array described

---
+++ array-split.js
@@ -0,0 +1,7 @@
+const array = [1,2,3,4,5,6,7,8,9];
+
+const array_slice = array.slice(1,3);
+const array_splice = array.splice(1,2,77,78);
+console.log(array_slice);
+console.log(array_splice);
+console.log(array);
